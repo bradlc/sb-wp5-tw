eb8a67303dff865add2e8cb8a297dfb005c626af
Gerrit Alex <admin@gerritalex.de>
2021-05-20 01:19:17 +0200

    init

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  mode: "jit",
+  purge: ["./pages/**/*.js", "./stories/**/*.js"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: "#FF0000",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
